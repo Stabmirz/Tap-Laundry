aaf9dafc72b2284b3b171b268c128421e29a1ab6
Mirza <amirza.techlaunch.io>
2019-07-03 12:43:20 -0400

    updated

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 function popup(){
 
     var htmlContent = document.createElement("div");
-    htmlContent.innerHTML = "Unit:<br><input type='text' value='ex. \"lb\",\"item\",or \"bag\"'><br>Cost per Unit:<br><input type='text' value='0.00'><br>Cost per Unit for Rush Orders:<br><input type='text' value='0.00'><br>Driver Pay per Unit:<br><input type='text' value='0.00'><br><span>Driver Pay per Unit for Rush Orders:</span><br><input type='text' value='0.00'><br><p id='note'>Note:These changes will take effect only for orders created after the current date and time.</p>"
+    htmlContent.innerHTML = "<span class='label'>Unit</span><br><input type='text' value='ex. \"lb\",\"item\",or \"bag\"'><br><span class='label'>Cost per Unit</span><br><input type='text' value='ex. 0.00'><br><span class='label'>Cost per Unit for Rush Orders</span><br><input type='text' value='ex. 0.00'><br><span class='label'>Driver Pay per Unit</span><br><input type='text' value='ex. 0.00'><br><span class='label'>Driver Pay per Unit for Rush Orders</span><br><input type='text' value='ex. 0.00'><br><p id='note'>Note: These changes will take effect only for orders created after the current date and time.</p>"
     swal({  
         title: 'New Global Price',
         content: htmlContent,
@@ -14,44 +14,4 @@ function popup(){
     })
         
 }
-//     swal({  
-//         title: 'New Global Price',
-//         content: {
-//         text: 'Unit',
-//         element: 'input',
-//         attributes: {
-//             placeholder: 'ex. "lb","item",or "bag"',
-//         },
-//         },
-//         content: {
-//         text: 'Cost per Unit',
-//         element: 'input',
-//             attributes: {
-//                 placeholder: 'ex. 0.00',
-//             },
-//         },
-//         content: {
-//         text: 'Cost per Unit for Rush Orders',
-//         element: 'input',
-//             attributes: {
-//                 placeholder: 'ex. 0.00',
-//             },
-//         },
-//         content: {
-//         text: 'Driver Pay per Unit',
-//         element: 'input',
-//             attributes: {
-//                 placeholder: 'ex. 0.00',
-//             },
-//         },
-//         content: {
-//         text: 'Driver Pay per Unit for Rush Orders',
-//         element: 'input',
-//             attributes: {
-//                 placeholder: 'ex. 0.00',
-//             },
-//         },
-//         buttons: ['Cancel', 'OK'],
-//     })
-        
-// }
+
