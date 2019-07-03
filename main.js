f4ef9d30f3cc00d7bb4d6d48312fab3eaad92880
Mirza <amirza.techlaunch.io>
2019-07-03 14:37:55 -0400

    updated

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ $(document).ready(function() {
 
 function popup(){
 
-    var htmlContent = document.createElement("div");
+    let htmlContent = document.createElement("div");
     htmlContent.innerHTML = "<span class='label'>Unit</span><br><input type='text' value='ex. \"lb\",\"item\",or \"bag\"'><br><span class='label'>Cost per Unit</span><br><input type='text' value='ex. 0.00'><br><span class='label'>Cost per Unit for Rush Orders</span><br><input type='text' value='ex. 0.00'><br><span class='label'>Driver Pay per Unit</span><br><input type='text' value='ex. 0.00'><br><span class='label'>Driver Pay per Unit for Rush Orders</span><br><input type='text' value='ex. 0.00'><br><p id='note'>Note: These changes will take effect only for orders created after the current date and time.</p>"
     swal({  
         title: 'New Global Price',
         content: htmlContent,
         buttons: ['Cancel', 'OK'],
+        closeOnClickOutside: false
     })
         
 }
